1a7c8068dc7ff89e00b4581fc0beba2f8363f7f3
Andrey Kashapov <kashapovandrey@gmail.com>
2018-03-09 21:26:08 +0300

    sudoku v.0.5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  
+  var resMatrix = matrix;
+
+  function checkZero(resMatrix) {
+    for (var i = 0; i < resMatrix.length; i++) {
+      for (var j = 0; j < resMatrix[i].length; j++) {
+        if (matrix[i][j] == 0) {
+          resMatrix[i][j] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        }
+      }
+    }
+
+    return resMatrix;
+  }
+
+
+  resMatrix = checkZero(resMatrix);
+ 
+  //console.log(matrix);
+  //console.log(resMatrix);
+
+  return resMatrix;
 }
